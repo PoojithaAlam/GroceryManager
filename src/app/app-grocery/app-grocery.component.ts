3abd4fea92e8f9d539f69866a0536091d3fcaa5a
PoojithaAlam <poojithaalam@gmail.com>
2023-12-18 13:20:45 -0800

    Format

--- src/app/app-grocery/app-grocery.component.ts
+++ src/app/app-grocery/app-grocery.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-app-grocery',
   templateUrl: './app-grocery.component.html',
-  styleUrls: ['../../assets/bootstrap-5.3.2-dist/css/bootstrap.min.css','app-grocery.component.css'],
+  styleUrls: [
+    '../../assets/bootstrap-5.3.2-dist/css/bootstrap.min.css',
+    'app-grocery.component.css',
+  ],
 })
 export class AppGroceryComponent {
   task: string = '';
   tasks: { name: string; strike: boolean }[] = []; // Define tasks as an array of objects with 'name' and 'strike' properties
 
   deleteItem(task: string): void {
-    const index = this.tasks.findIndex((task: { name: any; }) => task.name === task.name);
+    const index = this.tasks.findIndex(
+      (task: { name: any }) => task.name === task.name
+    );
     if (index !== -1) {
       this.tasks.splice(index, 1);
     }
@@ -23,38 +28,22 @@ export class AppGroceryComponent {
   }
   editForm: FormGroup | undefined;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
-    this.editForm = this.formBuilder.group({})
+    this.editForm = this.formBuilder.group({});
     this.strikeElement();
-    }
-
-strikeElement(): void {
-  const listGroup = document.querySelector('.list-group');
-
-if(listGroup !==null)
-listGroup.addEventListener('click', (e: Event) => {
-      console.log("Button clicked");
-        const target = e.target as HTMLElement;
-        console.log(target.tagName)
-        if (target.tagName === 'LI') {
-          console.log("target is li");
-            target.classList.toggle('list-group-item')
-        }
-        
-    });
-  }   
-  
-   
-   
   }
 
-   
+  strikeElement(): void {
+    const listGroup = document.querySelector('.list-group');
 
-  
-
-  
-  
-
-
+    if (listGroup !== null)
+      listGroup.addEventListener('click', (e: Event) => {
+        const target = e.target as HTMLElement;
+        if (target.tagName === 'LI') {
+          target.classList.toggle('list-group-item');
+        }
+      });
+  }
+}
